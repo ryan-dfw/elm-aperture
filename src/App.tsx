1d2da00627b41c39d879a37a13725398bda30b61
Ryan <mckevitt.r@gmail.com>
2024-02-16 16:12:01 -0600

    hooked up embedded IG feed to a simple display modelled directly within app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import './App.css'
+import './App.css';
 
-function App() {
+import React, { useEffect } from 'react';
 
-  return (
-    <>
-      <p>
-        Website
-      </p>
-    </>
-  )
+const App = () => {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://embedsocial.com/cdn/ht.js';
+        script.async = true;
+        document.head.appendChild(script);
+        return () => {
+            document.head.removeChild(script);
+        };
+    }, []);
+
+    return (
+        <div>
+            <div className="embedsocial-hashtag" data-ref="ee15b8a1b11a57118b3d7f3808f5783156c9669a"></div>
+        </div>
+    );
 }
 
-export default App
+export default App;
