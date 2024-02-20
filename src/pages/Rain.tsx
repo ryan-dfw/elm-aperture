64dcea94286f4a4e20571562610ccd67c2b07a11
Ryan <mckevitt.r@gmail.com>
2024-02-19 19:44:52 -0600

    added content to Rain's page

--- src/pages/Rain.tsx
+++ src/pages/Rain.tsx
@@ -1,18 +1,19 @@
+import React, {useEffect} from "react";
+
 const Rain = () => {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://embedsocial.com/cdn/ht.js';
+        script.async = true;
+        document.head.appendChild(script);
+        return () => {
+            document.head.removeChild(script);
+        };
+    }, []);
+
     return (
         <div>
-            <h1>Rain's Page</h1>
-            <h1>Rain's Page</h1>
-            <h1>Rain's Page</h1>
-            <h1>Rain's Page</h1>
-            <h1>Rain's Page</h1>
-            <h1>Rain's Page</h1>
-            <h1>Rain's Page</h1>
-            <h1>Rain's Page</h1>
-            <h1>Rain's Page</h1>
-            <h1>Rain's Page</h1>
-            <h1>Rain's Page</h1>
-            <h1>Rain's Page</h1>
+            <div className="embedsocial-hashtag" data-ref="8384500192fa9438d90085d33cdda14329c0c3e4"></div>
         </div>
     );
 }
