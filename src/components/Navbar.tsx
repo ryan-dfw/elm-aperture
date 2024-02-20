cb0938abe7add0346a8c30925af56a4465836239
Ryan <mckevitt.r@gmail.com>
2024-02-19 21:55:37 -0600

    achieved dark dropdown menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,3 @@
-// Navbar.tsx
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { Nav, Navbar as BootstrapNavbar, NavDropdown } from 'react-bootstrap';
 import '../styles/Navbar.css';
@@ -15,18 +13,9 @@ const Navbar = () => {
         >
             <div className="container">
                 <Link to="/" className="navbar-brand">Elm Aperture</Link>
-                <BootstrapNavbar.Toggle aria-controls="navmenu" />
+                <BootstrapNavbar.Toggle aria-controls="navmenu"/>
                 <BootstrapNavbar.Collapse id="navmenu">
-                    <Nav className="ms-auto">
-                        <Nav.Item>
-                            <Link to="/home" className="nav-link smallify">Home</Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Link to="/calendar" className="nav-link smallify">Calendar</Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Link to="/gallery" className="nav-link smallify">Gallery</Link>
-                        </Nav.Item>
+                    <Nav className="ms-auto"  data-bs-theme="dark">
                         <NavDropdown title="Our Photographers" id="navbarDropdownMenuLink">
                             <NavDropdown.Item as={Link} to="/rain">Rain</NavDropdown.Item>
                             <NavDropdown.Item href="./meraki.html">Meraki</NavDropdown.Item>
