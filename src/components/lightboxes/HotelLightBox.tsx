cd856450a19f680e22871b8b4cb3b7ac95775190
Ryan <mckevitt.r@gmail.com>
2024-03-07 13:44:28 -0600

    styled image text on hover

--- src/components/lightboxes/HotelLightBox.tsx
+++ src/components/lightboxes/HotelLightBox.tsx
@@ -4,24 +4,24 @@ const HotelLightBox = () => {
     return (
         <div className="gallery-container">
             <div className="gallery">
-                <div><img src="https://picsum.photos/600/600/?image=512"/><a href="#lightbox-1">512</a></div>
-                <div><img src="https://picsum.photos/600/600/?image=513"/><a href="#lightbox-2">513</a></div>
-                <div><img src="https://picsum.photos/600/600/?image=514"/><a href="#lightbox-3">514</a></div>
-                <div><img src="https://picsum.photos/600/600/?image=515"/><a href="#lightbox-4">515</a></div>
-                <div><img src="https://picsum.photos/600/600/?image=516"/><a href="#lightbox-5">516</a></div>
-                <div><img src="https://picsum.photos/600/600/?image=517"/><a href="#lightbox-6">517</a></div>
-                <div><img src="https://picsum.photos/600/600/?image=518"/><a href="#lightbox-7">518</a></div>
-                <div><img src="https://picsum.photos/600/600/?image=519"/><a href="#lightbox-8">519</a></div>
-                <div><img src="https://picsum.photos/600/600/?image=520"/><a href="#lightbox-9">520</a></div>
-                <div><img src="https://picsum.photos/600/600/?image=521"/><a href="#lightbox-10">521</a></div>
-                <div><img src="https://picsum.photos/600/600/?image=522"/><a href="#lightbox-11">522</a></div>
-                <div><img src="https://picsum.photos/600/600/?image=523"/><a href="#lightbox-12">523</a></div>
-                <div><img src="https://picsum.photos/600/600/?image=524"/><a href="#lightbox-13">524</a></div>
-                <div><img src="https://picsum.photos/600/600/?image=525"/><a href="#lightbox-14">525</a></div>
-                <div><img src="https://picsum.photos/600/600/?image=526"/><a href="#lightbox-15">526</a></div>
-                <div><img src="https://picsum.photos/600/600/?image=527"/><a href="#lightbox-16">527</a></div>
-                <div><img src="https://picsum.photos/600/600/?image=528"/><a href="#lightbox-17">528</a></div>
-                <div><img src="https://picsum.photos/600/600/?image=529"/><a href="#lightbox-18">529</a></div>
+                <div><img src="https://picsum.photos/600/600/?image=512"/><a href="#lightbox-1">filler image</a></div>
+                <div><img src="https://picsum.photos/600/600/?image=513"/><a href="#lightbox-2">filler image</a></div>
+                <div><img src="https://picsum.photos/600/600/?image=514"/><a href="#lightbox-3">filler image</a></div>
+                <div><img src="https://picsum.photos/600/600/?image=515"/><a href="#lightbox-4">filler image</a></div>
+                <div><img src="https://picsum.photos/600/600/?image=516"/><a href="#lightbox-5">filler image</a></div>
+                <div><img src="https://picsum.photos/600/600/?image=517"/><a href="#lightbox-6">filler image</a></div>
+                <div><img src="https://picsum.photos/600/600/?image=518"/><a href="#lightbox-7">filler image</a></div>
+                <div><img src="https://picsum.photos/600/600/?image=519"/><a href="#lightbox-8">filler image</a></div>
+                <div><img src="https://picsum.photos/600/600/?image=520"/><a href="#lightbox-9">filler image</a></div>
+                <div><img src="https://picsum.photos/600/600/?image=521"/><a href="#lightbox-10">filler image</a></div>
+                <div><img src="https://picsum.photos/600/600/?image=522"/><a href="#lightbox-11">filler image</a></div>
+                <div><img src="https://picsum.photos/600/600/?image=523"/><a href="#lightbox-12">filler image</a></div>
+                <div><img src="https://picsum.photos/600/600/?image=524"/><a href="#lightbox-13">filler image</a></div>
+                <div><img src="https://picsum.photos/600/600/?image=525"/><a href="#lightbox-14">filler image</a></div>
+                <div><img src="https://picsum.photos/600/600/?image=526"/><a href="#lightbox-15">filler image</a></div>
+                <div><img src="https://picsum.photos/600/600/?image=527"/><a href="#lightbox-16">filler image</a></div>
+                <div><img src="https://picsum.photos/600/600/?image=528"/><a href="#lightbox-17">filler image</a></div>
+                <div><img src="https://picsum.photos/600/600/?image=529"/><a href="#lightbox-18">filler image</a></div>
             </div>
         </div>
     );
