89aef826293109148c3c829a69334594e1c34fca
Ryan <mckevitt.r@gmail.com>
2024-03-09 05:13:25 -0600

    added images to hotel page

--- src/components/lightboxes/HotelLightBox.tsx
+++ src/components/lightboxes/HotelLightBox.tsx
@@ -1,12 +1,42 @@
 import '../../styles/Lightbox.scss';
 import { useContextValue} from "../../contexts/Context.tsx";
-import {useCallback, useEffect} from "react";
+import {useCallback, useEffect, useState} from "react";
 
 const HotelLightBox = () => {
+    const imagesData = [
+        { src: 'res/img/realestate/hotels/hotel_01.webp', title: 'Hotel Bliss', detail: ', Kemah TX' },
+        { src: 'res/img/realestate/hotels/hotel_02.webp', title: 'OYO', detail: ', Corpus Christi TX' },
+        { src: 'res/img/realestate/hotels/hotel_03.webp', title: 'Hotel Bliss', detail: ', Kemah TX' },
+        { src: 'res/img/realestate/hotels/hotel_04.webp', title: 'Stamford Inn', detail: ', Stamford TX' },
+        { src: 'res/img/realestate/hotels/hotel_05.webp', title: 'Budget Inn', detail: ', Corpus Christi TX' },
+        { src: 'res/img/realestate/hotels/hotel_06.webp', title: 'Hotel Bliss', detail: ', Kemah TX' },
+        { src: 'res/img/realestate/hotels/hotel_07.webp', title: 'OYO', detail: ', Midland TX' },
+        { src: 'res/img/realestate/hotels/hotel_08.webp', title: 'Plaza Motel', detail: ', Corpus Christi TX' },
+        { src: 'res/img/realestate/hotels/hotel_09.webp', title: 'Vali Ho', detail: ', Weslaco TX' },
+        { src: 'res/img/realestate/hotels/hotel_10.webp', title: 'OYO', detail: ', Kingsville TX' },
+        { src: 'res/img/realestate/hotels/hotel_11.webp', title: 'OYO', detail: ', Kingsville TX' },
+        { src: 'res/img/realestate/hotels/hotel_12.webp', title: "America's Best Value Inn", detail: ', Wichita Falls TX' },
+        { src: 'res/img/realestate/hotels/hotel_13.webp', title: 'Hometown Inn', detail: ', Texhoma TX' },
+        { src: 'res/img/realestate/hotels/hotel_14.webp', title: 'Grand Eagle Ford Lodge', detail: ', Tilden TX' },
+        { src: 'res/img/realestate/hotels/hotel_15.webp', title: 'Monterrey Motel', detail: ', Corpus Christi TX' },
+        { src: 'res/img/realestate/hotels/hotel_16.webp', title: 'Stamford Inn', detail: ', Stamford TX' },
+        { src: 'res/img/realestate/hotels/hotel_17.webp', title: 'Budget Inn', detail: ', Corpus Christi TX' },
+        { src: 'res/img/realestate/hotels/hotel_18.webp', title: 'Hometown Inn', detail: ', Lakin, KS' },
+    ];
     const { setShouldShowNav } = useContextValue();
+    const [currentIndex, setCurrentIndex] = useState(0);
+
     const closeLightbox = useCallback(() => {
         setShouldShowNav(true);
     }, [setShouldShowNav]);
+
+    const handlePrev = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === 0 ? imagesData.length - 1 : prevIndex - 1));
+    };
+
+    const handleNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === imagesData.length - 1 ? 0 : prevIndex + 1));
+    };
 
     useEffect(() => {
         const galleryImages = document.querySelectorAll('.galleryimage');
@@ -23,11 +53,25 @@ const HotelLightBox = () => {
             closeButton.addEventListener('click', closeLightbox);
         }
         const handleKeyPress = (event: KeyboardEvent) => {
-            if (event.key === 'Escape') {
-                const closeButtons = document.querySelectorAll('.close');
-                if (closeButtons.length > 0) {
-                    const firstCloseButton = closeButtons[0] as HTMLAnchorElement;
-                    firstCloseButton.click();
+            const closeButtons = document.querySelectorAll('.close');
+
+            if (event.key === 'Escape' && closeButtons.length > 0) {
+                const firstCloseButton = closeButtons[0] as HTMLAnchorElement;
+                firstCloseButton.click();
+            } else if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
+                const currentHash = window.location.hash;
+                const currentIndex = parseInt(currentHash.substring(5), 10);
+
+                if (!isNaN(currentIndex)) {
+                    let newIndex;
+
+                    if (event.key === 'ArrowLeft') {
+                        newIndex = Math.max(1, currentIndex - 1);
+                    } else {
+                        newIndex = Math.min(imagesData.length, currentIndex + 1);
+                    }
+
+                    window.location.hash = `#img-${newIndex}`;
                 }
             }
         };
@@ -46,36 +90,19 @@ const HotelLightBox = () => {
             }
             document.removeEventListener('keydown', handleKeyPress);
         };
-    }, [closeLightbox, setShouldShowNav]);
+    }, [imagesData.length, closeLightbox, setShouldShowNav]);
 
-    const imagesData = [
-        { src: 'https://picsum.photos/600/600/?image=512', title: 'filler image 1' },
-        { src: 'https://picsum.photos/600/600/?image=513', title: 'filler image 2' },
-        { src: 'https://picsum.photos/600/600/?image=514', title: 'filler image 3' },
-        { src: 'https://picsum.photos/600/600/?image=515', title: 'filler image 4' },
-        { src: 'https://picsum.photos/600/600/?image=516', title: 'filler image 5' },
-        { src: 'https://picsum.photos/600/600/?image=517', title: 'filler image 6' },
-        { src: 'https://picsum.photos/600/600/?image=518', title: 'filler image 7' },
-        { src: 'https://picsum.photos/600/600/?image=519', title: 'filler image 8' },
-        { src: 'https://picsum.photos/600/600/?image=520', title: 'filler image 9' },
-        { src: 'https://picsum.photos/600/600/?image=521', title: 'filler image 10' },
-        { src: 'https://picsum.photos/600/600/?image=522', title: 'filler image 11' },
-        { src: 'https://picsum.photos/600/600/?image=523', title: 'filler image 12' },
-        { src: 'https://picsum.photos/600/600/?image=524', title: 'filler image 13' },
-        { src: 'https://picsum.photos/600/600/?image=525', title: 'filler image 14' },
-        { src: 'https://picsum.photos/600/600/?image=526', title: 'filler image 15' },
-        { src: 'https://picsum.photos/600/600/?image=527', title: 'filler image 16' },
-        { src: 'https://picsum.photos/600/600/?image=528', title: 'filler image 17' },
-        { src: 'https://picsum.photos/600/600/?image=529', title: 'filler image 18' },
-    ];
+    if (currentIndex + 1 - currentIndex === 0) {
+        console.log(currentIndex);
+    }
 
     return (
         <div className="gallery-container">
             <div className="gallery">
                 {imagesData.map((image, index) => (
                     <div className="galleryimage" key={index}>
-                        <img src={image.src} alt={`Gallery Image ${index + 1}`} />
-                        <a href={`#img-${index + 1}`}>filler image</a>
+                        <img src={image.src} alt={`Gallery Image ${index + 1}`} loading="lazy" />
+                        <a href={`#img-${index + 1}`}>{image.title}</a>
                     </div>
                 ))}
             </div>
@@ -83,9 +110,13 @@ const HotelLightBox = () => {
             {imagesData.map((image, index) => (
                 <div className="img" id={`img-${index + 1}`} key={`img-${index + 1}`}>
                     <div className="content">
-                        <img src={image.src.replace('600/600', '1920/1080')} alt={`Large Image ${index + 1}`}/>
+                        <img
+                            src={image.src.replace('600/600', '1920/1080')}
+                            alt={`Large Image ${index + 1}`}
+                            loading="lazy"
+                        />
                         <div className="title">
-                            No. <b>{index + 512}</b> from Picsum
+                            {image.title}{image.detail}
                         </div>
                         <a
                             id={`closeButton-${index + 1}`}
@@ -96,6 +127,13 @@ const HotelLightBox = () => {
                     </div>
                 </div>
             ))}
+
+            <button className="carousel-btn prev" onClick={handlePrev} style={{display: 'none'}}>
+                &lt;
+            </button>
+            <button className="carousel-btn next" onClick={handleNext} style={{display: 'none'}}>
+                &gt;
+            </button>
         </div>
     );
 };
