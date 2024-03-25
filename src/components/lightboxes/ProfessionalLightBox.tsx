85419470ec0740411c25072494e4a101b440bd46
Ryan <mckevitt.r@gmail.com>
2024-03-25 02:07:42 -0500

    added metadata for Professional and Headshots

--- src/components/lightboxes/ProfessionalLightBox.tsx
+++ src/components/lightboxes/ProfessionalLightBox.tsx
@@ -4,13 +4,13 @@ const Professional = () => {
     const directory = 'portrait';
     const subDirectory = 'professional';
     const details: [string, string, string, number, number][] = [
+        ['Jackie', ', voice actor', 'Rain', 150, 220],
+        ['Gabriel', ', actor', 'Rain', 0, 120],
+        ['Chris', ', realtor', 'Rain', 130, 320],
+        ['Erik', ', teacher', 'Rain', 20, 250],
         ['filler image', '', 'no one', 20, 60],
         ['filler image', '', 'no one', 20, 60],
-        ['filler image', '', 'no one', 20, 60],
-        ['filler image', '', 'no one', 20, 60],
-        ['filler image', '', 'no one', 20, 60],
-        ['filler image', '', 'no one', 20, 60],
-        ['filler image', '', 'no one', 20, 60],
+        ['Dayle', ', therapist', 'no one', 20, 100],
         ['filler image', '', 'no one', 20, 60],
         ['filler image', '', 'no one', 20, 60],
         ['filler image', '', 'no one', 20, 60],
