006159bd5812e0b36d46e58c0c8400b7aa53c017
Ryan <mckevitt.r@gmail.com>
2024-03-30 19:39:08 -0500

    added Zadie to headshots

--- src/components/lightboxes/HeadshotLightBox.tsx
+++ src/components/lightboxes/HeadshotLightBox.tsx
@@ -4,10 +4,10 @@ const Headshot = () => {
     const directory = 'portrait';
     const subDirectory = 'headshot';
     const details: [string, string, string, number, number][] = [
-        ['Annie', '', 'Rain', 300, 0],
-        ['Allison', '', 'Rain', 450, 0],
-        ['Angel', '', 'Rain', 180, 0],
-        ['filler image', '', 'no one', 0, 0],
+        ['Annie', ', from Light of the World', 'Rain', 300, 0],
+        ['Allison', ', from Light of the World', 'Rain', 450, 0],
+        ['Angel', ', from Light of the World', 'Rain', 180, 0],
+        ['Zadie', ', from Light of the World', 'Rain', 100, 0],
         ['filler image', '', 'no one', 0, 0],
         ['filler image', '', 'no one', 0, 0],
         ['filler image', '', 'no one', 0, 0],
