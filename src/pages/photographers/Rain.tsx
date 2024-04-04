3daf06d7f546dddcc5bea7f25d72c11fdaf7dade
Ryan <mckevitt.r@gmail.com>
2024-04-04 02:51:58 -0500

    added vimeo video player to Rain's page

--- src/pages/photographers/Rain.tsx
+++ src/pages/photographers/Rain.tsx
@@ -4,9 +4,15 @@ import RainProfile from "../../components/profiles/RainProfile.tsx";
 const Rain = () => {
     return (
         <>
-            <RainProfile />
+            <RainProfile/>
             <div className="behold-container">
                 <BeholdWidget feedId="53ACNRUgJYRQEQ2FmFSt"/>
+            </div>
+            <div style={{margin: '50px'}}></div>
+            <div style={{padding: '56.25% 0 0 0', position: 'relative'}}>
+                <iframe src='https://vimeo.com/showcase/11079325/embed' title="Rain Showcase" allowFullScreen
+                        frameBorder='0'
+                        style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', borderRadius: '15px'}}></iframe>
             </div>
         </>
     )
