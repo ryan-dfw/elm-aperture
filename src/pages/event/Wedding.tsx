337425061f1205cc451bea1a314853841f1b4f88
Ryan <mckevitt.r@gmail.com>
2024-05-21 16:47:13 -0500

    added weddings video sampler beneath weddings photo portfolio

--- src/pages/event/Wedding.tsx
+++ src/pages/event/Wedding.tsx
@@ -3,7 +3,19 @@ import WeddingLightBoxWithStyles from "../../datasets/lightboxes/WeddingLightBox
 const Wedding = () => {
         return (
             <>
-                    <WeddingLightBoxWithStyles/>
+                <WeddingLightBoxWithStyles/>
+                <div className={"portfolio-section-header"}>
+                    <div className={"horizontal-line"}></div>
+                    <h4>WEDDING VIDEOGRAPHY</h4>
+                    <div className={"horizontal-line"}></div>
+                </div>
+                <div style={{margin: '50px'}}></div>
+                <div style={{padding: '56.25% 0 0 0', position: 'relative'}}>
+                    <iframe className={"vimeo-profile-frame"}
+                            src='https://vimeo.com/showcase/11121389/embed'
+                            title="Rain Showcase" allowFullScreen
+                    ></iframe>
+                </div>
             </>
         );
 }
