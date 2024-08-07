d43b0cb0c01460cd99e71ee365728ded6eb708a0
Ryan McKevitt <mckevitt.r@gmail.com>
2024-08-06 20:19:13 -0500

    swapped photo + metadata in events 16 and hotels 2,3,4,14,17. Commented numbers in line for events & hotels page for easier code readability.

--- src/datasets/lightboxes/EventsLightBox.tsx
+++ src/datasets/lightboxes/EventsLightBox.tsx
@@ -4,23 +4,41 @@ const Events = () => {
     const directory = 'events';
     const subDirectory = 'events';
     const details: [string, string, string, number, number][] = [
+        // 01
         ['Company Meeting', ', Fort Worth', 'Maivy', -30, -20],
+        // 02
         ['Oil Rig Visit', ', West Texas', 'Rain', 0, 0],
+        // 03
         ['Site Inspection', ', West Texas', 'Maivy', 0, 50],
+        // 04
         ['Guided Tour', ', Dallas', 'Rain', 0, 60],
+        // 05
         ['Graduation Party', ', Irving', 'Maivy', 0, 70],
+        // 06
         ['Graduation Party', ', Irving', 'Rain', 10, 90],
+        // 07
         ['Mai Colachi', ', Carrollton', 'Rain', 0, 60],
+        // 08
         ['Botswana Independence Day', ', Argyle', 'Rain', 10, 0],
+        // 09
         ['Botswana Independence Day', ', Argyle', 'Rain', 0, 0],
+        // 10
         ['Roots Remain', ", Big Rob's", 'Maivy', 10, 90],
+        // 11
         ['Roots Remain', ", Big Rob's", 'Rain', 10, 100],
+        // 12
         ['Eshtadur', ", Big Rob's", 'Maivy', 10, 120],
+        // 13
         ['Mogul Red Carpet', ', Irving', 'Maivy', 10, 140],
+        // 14
         ['Rooftop Birthday', ', Dallas', 'Rain', 10, 20],
+        // 15
         ['Mogul Red Carpet', ', Irving', 'Rain', 30, 130],
-        ['Mogul Red Carpet', ', Irving', 'Rain', 30, 130],
+        // 16
+        ['Engagement Party', ', Mi Cocina', 'Rain', 30, 130],
+        // 17
         ['Mogul Red Carpet', ', Irving', 'Rain', 20, 100],
+        //18
         ['Retirement Party', ', Euless', 'Rain', 40, 150]
     ];
 
--- src/datasets/lightboxes/HotelLightBox.tsx
+++ src/datasets/lightboxes/HotelLightBox.tsx
@@ -4,23 +4,41 @@ const Hotel = () => {
     const directory = 'realestate';
     const subDirectory = 'hotel';
     const details: [string, string, string, number, number][] = [
+        // 01
         ['Hotel Bliss', ', Kemah TX', 'Rain', 0, 0],
+        // 02
+        ['Holiday Inn Express', ', Schulenberg, TX', 'Rain', 0, 0],
+        // 03
+        ['Stamford Inn', ', Stamford TX', 'Rain', 0, 0],
+        // 04
         ['OYO', ', Corpus Christi TX', 'Rain', 0, 0],
-        ['Hotel Bliss', ', Kemah TX', 'Rain', 0, 0],
-        ['Stamford Inn', ', Stamford TX', 'Rain', 0, 0],
+        // 05
         ['Budget Inn', ', Corpus Christi TX', 'Rain', 0, 0],
+        // 06
         ['Hotel Bliss', ', Kemah TX', 'Rain', 0, 0],
+        // 07
         ['OYO', ', Midland TX', 'Rain', 0, 0],
+        // 08
         ['Plaza Motel', ', Corpus Christi TX', 'Rain', 0, 0],
+        // 09
         ['Vali Ho', ', Weslaco TX', 'Rain', 0, 0],
+        // 10
         ['OYO', ', Kingsville TX', 'Rain', 0, 0],
+        // 11
         ['OYO', ', Kingsville TX', 'Rain', 0, 0],
+        // 12
         ["America's Best Value Inn", ', Wichita Falls TX', 'Rain', 0, 0],
+        // 13
         ['Hometown Inn', ', Texhoma TX', 'Rain', 0, 0],
-        ['Grand Eagle Ford Lodge', ', Tilden TX', 'Rain', 0, 0],
+        // 14
+        ['Holiday Inn Express', ', Schulenberg, TX', 'Rain', 0, 0],
+        // 15
         ['Monterrey Motel', ', Corpus Christi TX', 'Rain', 0, 0],
+        // 16
         ['Stamford Inn', ', Stamford TX', 'Rain', 0, 0],
-        ['Budget Inn', ', Corpus Christi TX', 'Rain', 0, 0],
+        // 17
+        ['Holiday Inn Express', ', Schulenberg, TX', 'Rain', 0, 0],
+        // 18
         ['Hometown Inn', ', Lakin, KS', 'Rain', 0, 0]
     ];
 
