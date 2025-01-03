e0fc8a1d4dbd28141f849a441385bf73038802c7
Ryan McKevitt <mckevitt.r@gmail.com>
2025-01-03 17:27:38 -0600

    removed all references to vimeo

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -15,18 +15,18 @@ const Homepage = () => {
             <div className="behold-container">
                 <BeholdWidget feedId="yh5fXOj29q5dsG731Dtb"/>
             </div>
-            <div className={"portfolio-section-header"}>
-                <div className={"horizontal-line"}></div>
-                <h4>FEATURED VIDEOGRAPHY</h4>
-                <div className={"horizontal-line"}></div>
-            </div>
-            <div style={{margin: '50px'}}></div>
-            <div style={{padding: '56.25% 0 0 0', position: 'relative'}}>
-                <iframe className={"vimeo-profile-frame"}
-                        src='https://vimeo.com/showcase/11079325/embed'
-                        title="Elm Aperture Showcase" allowFullScreen
-                ></iframe>
-            </div>
+            {/*<div className={"portfolio-section-header"}>*/}
+            {/*    <div className={"horizontal-line"}></div>*/}
+            {/*    <h4>FEATURED VIDEOGRAPHY</h4>*/}
+            {/*    <div className={"horizontal-line"}></div>*/}
+            {/*</div>*/}
+            {/*<div style={{margin: '50px'}}></div>*/}
+            {/*<div style={{padding: '56.25% 0 0 0', position: 'relative'}}>*/}
+            {/*    <iframe className={"vimeo-profile-frame"}*/}
+            {/*            src='https://vimeo.com/showcase/11079325/embed'*/}
+            {/*            title="Elm Aperture Showcase" allowFullScreen*/}
+            {/*    ></iframe>*/}
+            {/*</div>*/}
             <div className="booking-button-container">
                 <BookingButton/>
             </div>
--- src/pages/photographers/Rain.tsx
+++ src/pages/photographers/Rain.tsx
@@ -23,23 +23,23 @@ const Rain = () => {
             <div className="behold-container">
                 <BeholdWidget feedId="53ACNRUgJYRQEQ2FmFSt"/>
             </div>
-            <div className={"portfolio-section-header"}>
-                <div className={"horizontal-line"}></div>
-                <h4>FEATURED VIDEOS ON VIMEO</h4>
-                <div className={"horizontal-line"}></div>
-            </div>
-            <div style={{margin: '50px'}}></div>
-            <div style={{padding: '56.25% 0 0 0', position: 'relative'}}>
-                <iframe className={"vimeo-profile-frame"}
-                        src='https://vimeo.com/showcase/11079325/embed'
-                        title="Rain Showcase" allowFullScreen
-                ></iframe>
-            </div>
-            <div className={"portfolio-section-header"}>
-                <div className={"horizontal-line"}></div>
-                <h4>AVAILABILITY </h4>
-                <div className={"horizontal-line"}></div>
-            </div>
+            {/*<div className={"portfolio-section-header"}>*/}
+            {/*    <div className={"horizontal-line"}></div>*/}
+            {/*    <h4>FEATURED VIDEOS ON VIMEO</h4>*/}
+            {/*    <div className={"horizontal-line"}></div>*/}
+            {/*</div>*/}
+            {/*<div style={{margin: '50px'}}></div>*/}
+            {/*<div style={{padding: '56.25% 0 0 0', position: 'relative'}}>*/}
+            {/*    <iframe className={"vimeo-profile-frame"}*/}
+            {/*            src='https://vimeo.com/showcase/11079325/embed'*/}
+            {/*            title="Rain Showcase" allowFullScreen*/}
+            {/*    ></iframe>*/}
+            {/*</div>*/}
+            {/*<div className={"portfolio-section-header"}>*/}
+            {/*    <div className={"horizontal-line"}></div>*/}
+            {/*    <h4>AVAILABILITY </h4>*/}
+            {/*    <div className={"horizontal-line"}></div>*/}
+            {/*</div>*/}
             <div style={{margin: '50px'}}></div>
             <div className='calendar-container'>
                 <RainCalendar fullscreen={false}/>
