56d2531a5850e68ad5578a209cf61933082f49e1
Ryan McKevitt <mckevitt.r@gmail.com>
2025-01-03 02:10:24 -0600

    on mobile, events directories now are displayed in two columns with 1:1 aspect ratio

--- src/datasets/lightboxes/EventsLightBox.tsx
+++ src/datasets/lightboxes/EventsLightBox.tsx
@@ -4,24 +4,24 @@ const Events = () => {
     const directory = 'events';
     const subDirectory = 'events';
     const details: [string, string, string, number, number][] = [
-        ['Company Meeting', ', Fort Worth', 'Maivy', -30, -20],             // 01
+        ['Company Meeting', ', Fort Worth', 'Maivy', -30, 0],               // 01
         ['Oil Rig Visit', ', West Texas', 'Rain', 0, 0],                    // 02
-        ['Site Inspection', ', West Texas', 'Maivy', 0, 50],                // 03
-        ['Guided Tour', ', Dallas', 'Rain', 0, 60],                         // 04
-        ['Graduation Party', ', Irving', 'Maivy', 0, 70],                   // 05
-        ['Graduation Party', ', Irving', 'Rain', 10, 90],                   // 06
-        ['Mai Colachi', ', Carrollton', 'Rain', 0, 60],                     // 07
+        ['Site Inspection', ', West Texas', 'Maivy', 0, 0],                 // 03
+        ['Guided Tour', ', Dallas', 'Rain', 0, 0],                          // 04
+        ['Graduation Party', ', Irving', 'Maivy', 0, 0],                    // 05
+        ['Graduation Party', ', Irving', 'Rain', 10, 0],                    // 06
+        ['Mai Colachi', ', Carrollton', 'Rain', 0, 0],                      // 07
         ['Botswana Independence Day', ', Argyle', 'Rain', 10, 0],           // 08
         ['Botswana Independence Day', ', Argyle', 'Rain', 0, 0],            // 09
-        ['Roots Remain', ", Big Rob's", 'Maivy', 10, 90],                   // 10
-        ['Roots Remain', ", Big Rob's", 'Rain', 10, 100],                   // 11
-        ['Eshtadur', ", Big Rob's", 'Maivy', 10, 120],                      // 12
-        ['Mogul Red Carpet', ', Irving', 'Maivy', 10, 140],                 // 13
-        ['Rooftop Birthday', ', Dallas', 'Rain', 10, 20],                   // 14
-        ['Mogul Red Carpet', ', Irving', 'Rain', 30, 130],                  // 15
-        ['Engagement Party', ', Mi Cocina', 'Rain', 30, 130],               // 16
-        ['Mogul Red Carpet', ', Irving', 'Rain', 20, 100],                  // 17
-        ['Retirement Party', ', Euless', 'Rain', 40, 150]                   // 18
+        ['Roots Remain', ", Big Rob's", 'Maivy', 10, 0],                    // 10
+        ['Roots Remain', ", Big Rob's", 'Rain', 10, 0],                     // 11
+        ['Eshtadur', ", Big Rob's", 'Maivy', 10, 0],                        // 12
+        ['Mogul Red Carpet', ', Irving', 'Maivy', 10, 0],                   // 13
+        ['Rooftop Birthday', ', Dallas', 'Rain', 10, 0],                    // 14
+        ['Mogul Red Carpet', ', Irving', 'Rain', 30, 0],                    // 15
+        ['Engagement Party', ', Mi Cocina', 'Rain', 30, 0],                 // 16
+        ['Mogul Red Carpet', ', Irving', 'Rain', 20, 0],                    // 17
+        ['Retirement Party', ', Euless', 'Rain', 40, 0]                     // 18
     ];
 
     return <LightBox  directory={directory} subDirectory={subDirectory} details={details}/>;
--- src/datasets/lightboxes/WeddingLightBox.tsx
+++ src/datasets/lightboxes/WeddingLightBox.tsx
@@ -4,24 +4,24 @@ const Wedding = () => {
     const directory = 'events';
     const subDirectory = 'wedding';
     const details: [string, string, string, number, number][] = [
-        ['Palos Verdes', ', Wedding', 'Rain', 0, 80],                       // 01
-        ['The Springs', ', Wedding Portraits', 'Rain', 0, -50],             // 02
-        ['Serene Lakeside', ', Wedding Party', 'Maivy', 0, -50],            // 03
-        ['Serene Lakeside', ', Wedding Portrait', 'Maivy', 0, 300],         // 04
-        ['Mandalay Canal', ', Engagement', 'Rain', 0, 40],                  // 05
-        ['Japanese Garden', ', Wedding Ceremony', 'Rain', 30, 80],          // 06
-        ['Japanese Garden', ', Wedding Portrait', 'Rain', 0, -30],          // 07
-        ['Japanese Garden', ', Wedding Portrait', 'Rain', 0, 40],           // 08
-        ['Garden Hall', ', Wedding Reception', 'Rain', 0, 20],              // 09
-        ['Thanksgiving Square', ', Wedding Ceremony', 'Rain', 20, 30],      // 10
-        ['Thanksgiving Square', ', Wedding Ceremony', 'Rain', 30, 40],      // 11
-        ['Thanksgiving Square', ', Wedding Ceremony', 'Rain', 30, 110],     // 12
-        ['Venue Nine-Twenty', ', Wedding Portrait', 'Rain', 30, 120],       // 13
-        ['Thanksgiving Square', ', Family Photos', 'Rain', 20, 60],         // 14
-        ['Thanksgiving Square', ', Wedding Portrait', 'Rain', 10, 100],     // 15
-        ['Thanksgiving Square', ', Wedding Portrait', 'Rain', -40, -180],   // 16
-        ['Pilgrim Rest Church', ', Wedding Ceremony', 'Maivy', 40, 100],    // 17
-        ['Pilgrim Rest Church', ', Wedding Ceremony', 'Maivy', 10, 80]      // 18
+        ['Palos Verdes', ', Wedding', 'Rain', 0, 0],                        // 01
+        ['The Springs', ', Wedding Portraits', 'Rain', 0, 0],               // 02
+        ['Serene Lakeside', ', Wedding Party', 'Maivy', 0, 0],              // 03
+        ['Serene Lakeside', ', Wedding Portrait', 'Maivy', 0, 0],           // 04
+        ['Mandalay Canal', ', Engagement', 'Rain', 0, 0],                   // 05
+        ['Japanese Garden', ', Wedding Ceremony', 'Rain', 30, 0],           // 06
+        ['Japanese Garden', ', Wedding Portrait', 'Rain', 0, 0],            // 07
+        ['Japanese Garden', ', Wedding Portrait', 'Rain', 0, 0],            // 08
+        ['Garden Hall', ', Wedding Reception', 'Rain', 0, 0],               // 09
+        ['Thanksgiving Square', ', Wedding Ceremony', 'Rain', 20, 0],       // 10
+        ['Thanksgiving Square', ', Wedding Ceremony', 'Rain', 30, 0],       // 11
+        ['Thanksgiving Square', ', Wedding Ceremony', 'Rain', 30, 0],       // 12
+        ['Venue Nine-Twenty', ', Wedding Portrait', 'Rain', 30, 0],         // 13
+        ['Thanksgiving Square', ', Family Photos', 'Rain', 20, 0],          // 14
+        ['Thanksgiving Square', ', Wedding Portrait', 'Rain', 10, 0],       // 15
+        ['Thanksgiving Square', ', Wedding Portrait', 'Rain', -40, 0],      // 16
+        ['Pilgrim Rest Church', ', Wedding Ceremony', 'Maivy', 40, 0],      // 17
+        ['Pilgrim Rest Church', ', Wedding Ceremony', 'Maivy', 10, 0]       // 18
     ];
 
     return <LightBox  directory={directory} subDirectory={subDirectory} details={details}/>;
