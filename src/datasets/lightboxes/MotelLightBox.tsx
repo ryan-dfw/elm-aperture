4ace20b767984b36e00a8bf97410a3c31e941155
Ryan McKevitt <mckevitt.r@gmail.com>
2025-04-02 21:00:04 -0500

    prepared main section of motels page w/ new photos & accurate descriptions

--- src/datasets/lightboxes/MotelLightBox.tsx
+++ src/datasets/lightboxes/MotelLightBox.tsx
@@ -4,24 +4,24 @@ const Motel = () => {
     const directory = 'realestate';
     const subDirectory = 'motel';
     const details: [string, string, string, number, number][] = [
-        ['Hotel Bliss', ', Kemah TX', 'Rain', 0, 0],                        // 01
-        ['Holiday Inn Express', ', Schulenberg, TX', 'Rain', 0, 0],         // 02
-        ['Stamford Inn', ', Stamford TX', 'Rain', 0, 0],                    // 03
-        ['OYO', ', Corpus Christi TX', 'Rain', 0, 0],                       // 04
-        ['Budget Inn', ', Corpus Christi TX', 'Rain', 0, 0],                // 05
-        ['Hotel Bliss', ', Kemah TX', 'Rain', 0, 0],                        // 06
-        ['OYO', ', Midland TX', 'Rain', 0, 0],                              // 07
-        ['Plaza Motel', ', Corpus Christi TX', 'Rain', 0, 0],               // 08
+        ['River Valley Motor Inn', ', La Grange TX', 'Rain', 0, 0],         // 01
+        ['Moulton Inn & Suites', ', Moulton TX', 'Rain', 0, 0],             // 02
+        ['OYO', ', Corpus Christi TX', 'Rain', 0, 0],                       // 03
+        ['River Valley Motor Inn', ', La Grange TX', 'Rain', 0, 0],         // 04
+        ['Crown Inn', ', Denver City TX', 'Rain', 0, 0],                    // 05
+        ['Regency Inn & Suites', ', Bay City TX', 'Rain', 0, 0],            // 06
+        ['Hotel Bliss', ', Kemah TX', 'Rain', 0, 0],                        // 07
+        ['Plaza Motel', ', Corpus Christi', 'Rain', 0, 0],                  // 08
         ['Vali Ho', ', Weslaco TX', 'Rain', 0, 0],                          // 09
-        ['OYO', ', Kingsville TX', 'Rain', 0, 0],                           // 10
-        ['OYO', ', Kingsville TX', 'Rain', 0, 0],                           // 11
-        ["America's Best Value Inn", ', Wichita Falls TX', 'Rain', 0, 0],   // 12
-        ['Hometown Inn', ', Texhoma TX', 'Rain', 0, 0],                     // 13
-        ['Holiday Inn Express', ', Schulenberg, TX', 'Rain', 0, 0],         // 14
-        ['Monterrey Motel', ', Corpus Christi TX', 'Rain', 0, 0],           // 15
-        ['Stamford Inn', ', Stamford TX', 'Rain', 0, 0],                    // 16
-        ['Holiday Inn Express', ', Schulenberg, TX', 'Rain', 0, 0],         // 17
-        ['Hometown Inn', ', Lakin, KS', 'Rain', 0, 0]                       // 18
+        ['Budget Inn', ', Corpus Christ TX', 'Rain', 0, 0],                 // 10
+        ['Island Inn', ', Galveston TX', 'Rain', 0, 0],                     // 11
+        ['Best Value Inn', ', Wichita Falls TX', 'Rain', 0, 0],             // 12
+        ['Hometown Inn', ', Texhoma OK', 'Rain', 0, 0],                     // 13
+        ['Executive Inn', ', Pleasanton TX', 'Rain', 0, 0],                 // 14
+        ['Budget Inn', ', Corpus Christ TX', 'Rain', 0, 0],                 // 15
+        ['Texas Inn', ', San Benito TX', 'Rain', 0, 0],                     // 16
+        ['Stamford Inn', ', Stamford TX', 'Rain', 0, 0],                    // 17
+        ['Moulton Inn & Suites', ', Moulton TX', 'Rain', 0, 0]              // 18
     ];
 
     return <LightBox  directory={directory} subDirectory={subDirectory} details={details}/>;
