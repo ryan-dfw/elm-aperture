16c3757338150d328821cc99f5a872297e8c564b
Ryan McKevitt <mckevitt.r@gmail.com>
2026-05-27 16:10:56 -0500

    shuffled motel thumbnail order according to survey results

--- src/content/galleries/MotelGallery.tsx
+++ src/content/galleries/MotelGallery.tsx
@@ -6,18 +6,18 @@ const Motel = () => {
     const layout = 'landscape';
     const details: [string, string, string, number, number][] = [
         ['River Valley Motor Inn', ', La Grange TX', 'Rain', 0, 0],         // 01
-        ['Moulton Inn & Suites', ', Moulton TX', 'Rain', 0, 0],             // 02
+        ['Hotel Bliss', ', Kemah TX', 'Rain', 0, 0],                        // 02
         ['OYO', ', Corpus Christi TX', 'Rain', 0, 0],                       // 03
-        ['River Valley Motor Inn', ', La Grange TX', 'Rain', 0, 0],         // 04
+        ['Best Value Inn', ', Wichita Falls TX', 'Rain', 0, 0],             // 04
         ['Crown Inn', ', Denver City TX', 'Rain', 0, 0],                    // 05
-        ['Regency Inn & Suites', ', Bay City TX', 'Rain', 0, 0],            // 06
-        ['Hotel Bliss', ', Kemah TX', 'Rain', 0, 0],                        // 07
+        ['Hometown Inn', ', Texhoma OK', 'Rain', 0, 0],                     // 06
+        ['Regency Inn & Suites', ', Bay City TX', 'Rain', 0, 0],            // 07
         ['Plaza Motel', ', Corpus Christi', 'Rain', 0, 0],                  // 08
         ['Vali Ho', ', Weslaco TX', 'Rain', 0, 0],                          // 09
         ['Budget Inn', ', Corpus Christ TX', 'Rain', 0, 0],                 // 10
         ['Island Inn', ', Galveston TX', 'Rain', 0, 0],                     // 11
-        ['Best Value Inn', ', Wichita Falls TX', 'Rain', 0, 0],             // 12
-        ['Hometown Inn', ', Texhoma OK', 'Rain', 0, 0],                     // 13
+        ['River Valley Motor Inn', ', La Grange TX', 'Rain', 0, 0],         // 12
+        ['Moulton Inn & Suites', ', Moulton TX', 'Rain', 0, 0],             // 13
         ['Executive Inn', ', Pleasanton TX', 'Rain', 0, 0],                 // 14
         ['Budget Inn', ', Corpus Christ TX', 'Rain', 0, 0],                 // 15
         ['Texas Inn', ', San Benito TX', 'Rain', 0, 0],                     // 16
